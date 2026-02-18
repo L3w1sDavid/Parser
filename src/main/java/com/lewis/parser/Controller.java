e04ebfce5f805994b3448ad2327d3ea00a4162ba
Lewis Oluwapamilerinayo <erinayolewis@gmail.com>
2026-02-18 13:23:52 +0100

    Changed map to DTO, Not really sure how to change from maven to gradle.

--- src/main/java/com/lewis/parser/Controller.java
+++ src/main/java/com/lewis/parser/Controller.java
@@ -3,16 +3,13 @@ package com.lewis.parser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Map;
 @RestController
 public class Controller {
     @GetMapping("/api/whoami")
-    public Map<String, String> parseHeaders(HttpServletRequest request) {
-        // FCC expects these exact three keys: ipaddress, language, and software
-        return Map.of(
-                "ipaddress", request.getRemoteAddr(),
-                "language", request.getHeader("Accept-Language"),
-                "software", request.getHeader("User-Agent")
+    public WhoamIResponse parseHeaders(HttpServletRequest request) {
+        WhoamIResponse whoamIResponse = new WhoamIResponse(request.getRemoteAddr(), request.getHeader("Accept-Language"), request.getHeader("User-Agent")
         );
+        return whoamIResponse;
+
     }
 }
